dc09c5c39f62cc03d85e6c875d01c8b250cc7a5c
GayatriXJadhav <gayatrijadhav1202@gmail.com>
2025-09-28 00:07:24 +0530

    Some changes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const http = require("http");
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
 const server = http.createServer(app);
 const io = new Server(server, {
   cors:
